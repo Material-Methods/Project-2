57f2ec517ca5380624586e1de924fbd05bbbaf26
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2018-04-02 16:06:22 -0500

    Added js for hover

---
+++ js/site.js
@@ -0,0 +1,11 @@
+(function () {
+    var proj_list = document.getElementById("nav-proj-list");
+    proj_list.style.display = "none";
+    var proj_nav = document.getElementById("nav-pro");
+    proj_nav.addEventListener('mouseenter', function () {
+        proj_list.style.display = "block";
+    });
+    proj_nav.addEventListener('mouseleave', function () {
+        proj_list.style.display = "none";
+    });
+})();
