5fd42e43d040ffd3c8f45eaa00fb62e5edaac8e6
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2018-04-22 13:39:52 -0500

    Added js for accordion

--- js/site.js
+++ js/site.js
@@ -1,4 +1,5 @@
 (function () {
+
     var proj_list = document.getElementById("nav-proj-list");
     proj_list.style.display = "none";
     var proj_nav = document.getElementById("nav-pro");
@@ -8,4 +9,18 @@
     proj_nav.addEventListener('mouseleave', function () {
         proj_list.style.display = "none";
     });
+
+    var expandables = document.getElementsByClassName("accordion");
+    for (var i = 0; i < expandables.length; i++) {
+        expandables[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
+
 })();
