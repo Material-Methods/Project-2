24eecf53436eb9e66b9d1efa37000a1c77afe550
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2018-04-22 13:50:28 -0500

    fiing navigation

--- js/site.js
+++ js/site.js
@@ -1,15 +1,5 @@
 (function () {
 
-    var proj_list = document.getElementById("nav-proj-list");
-    proj_list.style.display = "none";
-    var proj_nav = document.getElementById("nav-pro");
-    proj_nav.addEventListener('mouseenter', function () {
-        proj_list.style.display = "flex";
-    });
-    proj_nav.addEventListener('mouseleave', function () {
-        proj_list.style.display = "none";
-    });
-
     var expandables = document.getElementsByClassName("accordion");
     for (var i = 0; i < expandables.length; i++) {
         expandables[i].addEventListener("click", function() {
